f57f7dfe2ccfe1661d9b48f40995f74190622225
MladoniSzabi <szabimlado@gmail.com>
2025-10-09 13:48:23 +0100

    added proper authentication.

--- main.py
+++ main.py
@@ -1,50 +1,63 @@
 import miro_api
 import dotenv
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, make_response
+
+from functools import wraps
 
 dotenv.load_dotenv()
 app = Flask(__name__)
 
-miro = miro_api.Miro()
 
-access_token = None
+def auth_required(func):
+    @wraps(func)
+    def inner(*args, **kwargs):
+        if request.cookies.get("miro_token") == None:
+            miro = miro_api.Miro()
+            return redirect(miro.auth_url)
+        try:
+            result = func(*args, **kwargs)
+            return result
+        except miro_api.exceptions.UnauthorizedException:
+            miro = miro_api.Miro()
+            return redirect(miro.auth_url)
+    return inner
 
 
 @app.route("/")
+@auth_required
 def index():
-    if not miro.is_authorized:
-        return redirect(miro.auth_url)
     return ""
 
 
 @app.route("/miro/redirect/url")
 def miro_redirect():
     code = request.args.get("code")
-    state = request.args.get("state")
-    client_id = request.args.get("client_id")
-    team_id = request.args.get("team_id")
+    miro = miro_api.Miro()
     access_token = miro.exchange_code_for_access_token(code)
-    return f"<html><body><h1>Hello, world!</h1><p>Your access token is {access_token}</p></body></html>"
+    response = make_response(
+        "<html><body><h1>Hello, world!</h1></body></html>")
+    response.set_cookie("miro_token", access_token, secure=True)
+    return response
 
 
 @app.route("/get/board")
+@auth_required
 def get_board_frames():
-    if not miro.is_authorized:
-        return redirect(miro.auth_url)
-    boards = miro.api.get_boards()
+    miro = miro_api.MiroApi(request.cookies.get("miro_token"))
+    boards = miro.get_boards()
     board_id = boards.data[0].id
-    frame_id = miro.api.get_items(board_id, type="frame").data[0].id
-    objects = miro.api.get_items_within_frame(board_id, frame_id)
+    frame_id = miro.get_items(board_id, type="frame").data[0].id
+    objects = miro.get_items_within_frame(board_id, frame_id)
     return str(objects.data)
 
 
 @app.route("/get/connectors")
+@auth_required
 def get_connectors():
-    if not miro.is_authorized:
-        return redirect(miro.auth_url)
-    boards = miro.api.get_boards()
+    miro = miro_api.MiroApi(request.cookies.get("miro_token"))
+    boards = miro.get_boards()
     board_id = boards.data[0].id
-    connectors = miro.api.get_connectors(board_id)
+    connectors = miro.get_connectors(board_id)
     return str(connectors)
 
 
