73e18cd897102ae882432a80356da147e8edb202
MladoniSzabi <szabimlado@gmail.com>
2025-10-09 15:23:26 +0100

    added code to select which node to start creating the excel sheet from.

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import miro_api
 import dotenv
-from flask import Flask, request, redirect, make_response
+from flask import Flask, request, redirect, make_response, render_template, Response
 
+import json
 from functools import wraps
+from html.parser import HTMLParser
 
 dotenv.load_dotenv()
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    static_url_path="/static",
+    static_folder="static",
+    template_folder="templates"
+)
 
 
 def auth_required(func):
     @wraps(func)
     def inner(*args, **kwargs):
         if request.cookies.get("miro_token") == None:
+            if "text/json" in request.headers.get("Accept", ""):
+                return json.dumps({"error": 403})
             miro = miro_api.Miro()
             return redirect(miro.auth_url)
         try:
@@ -25,12 +34,12 @@ def auth_required(func):
 
 @app.route("/")
 @auth_required
-def index():
-    return ""
+def index() -> str:
+    return render_template("index.html")
 
 
 @app.route("/miro/redirect/url")
-def miro_redirect():
+def miro_redirect() -> Response:
     code = request.args.get("code")
     miro = miro_api.Miro()
     access_token = miro.exchange_code_for_access_token(code)
@@ -40,15 +49,70 @@ def miro_redirect():
     return response
 
 
-@app.route("/get/board")
+@app.route("/get/boards")
 @auth_required
-def get_board_frames():
+def get_boards() -> Response:
     miro = miro_api.MiroApi(request.cookies.get("miro_token"))
     boards = miro.get_boards()
-    board_id = boards.data[0].id
-    frame_id = miro.get_items(board_id, type="frame").data[0].id
+    retval = []
+    for board in boards.data:
+        retval.append({
+            "name": board.name,
+            "id": board.id
+        })
+    response = Response(json.dumps(retval), mimetype="text/json")
+    return response
+
+
+@app.route("/get/board/<string:board_id>/frames")
+@auth_required
+def get_frames(board_id: str) -> Response:
+    miro = miro_api.MiroApi(request.cookies.get("miro_token"))
+    frames = miro.get_items(board_id, type="frame")
+    retval = []
+    for frame in frames.data:
+        retval.append({
+            "name": frame.data.actual_instance.title,
+            "id": frame.id
+        })
+    response = Response(json.dumps(retval), mimetype="text/json")
+    return response
+
+
+def extract_data(html: str) -> str:
+    class NameExtractor(HTMLParser):
+        def handle_starttag(self, tag, attrs):
+            pass
+
+        def handle_endtag(self, tag):
+            pass
+
+        def handle_data(self, data):
+            self.collected += data + " "
+
+    parser = NameExtractor()
+    parser.collected = ""
+    parser.feed(html)
+    return parser.collected.strip()
+
+
+@app.route("/get/board/<string:board_id>/frame/<string:frame_id>/objects")
+def get_objects(board_id: str, frame_id: str) -> Response:
+    miro = miro_api.MiroApi(request.cookies.get("miro_token"))
     objects = miro.get_items_within_frame(board_id, frame_id)
-    return str(objects.data)
+    retval = []
+    for obj in objects.data:
+        retval.append({
+            "name": extract_data(obj.data.actual_instance.content),
+            "id": obj.id
+        })
+    response = Response(json.dumps(retval), mimetype="text/json")
+    return response
+
+
+@app.route("/get/board/<string:board_id>/frame/<string:frame_id>/object/<string:object_id>/excel")
+def get_excel(board_id: str, frame_id: str, object_id: str) -> Response:
+    pass
 
 
 @app.route("/get/connectors")
---
+++ static/script.js
@@ -0,0 +1,93 @@
+function downloadExcel(boardId, frameId, nodeId) {
+    fetch("/get/board/" + boardId + "/frame/" + frameId + "/object" + nodeId + "/excel")
+}
+
+function populateNodes(boardId, frameId, nodes) {
+    const startError = document.getElementById("start-error")
+    startError.innerHTML = ""
+    if (nodes.length == 0) {
+        startError.innerText = "You don't have any nodes in this frame"
+        return
+    }
+
+    const selectEl = document.getElementById("start")
+    selectEl.innerHTML = ""
+    let selectedNode = null
+    for (const node of nodes) {
+        if (selectedNode === null)
+            selectedNode = node.id
+        const option = document.createElement("option")
+        option.value = node.id
+        option.innerText = node.name
+        selectEl.appendChild(option)
+    }
+    document.getElementById("select-start").style.display = "block"
+
+    const runButton = document.getElementById("run")
+    runButton.style.display = "block"
+    runButton.onclick = () => {
+        downloadExcel(boardId, frameId, selectedNode)
+    }
+
+    selectEl.onchange = () => {
+        runButton.onclick = () => {
+            downloadExcel(boardId, frameId, selectEl.value)
+        }
+    }
+}
+
+function populateFrames(boardId, frames) {
+    const frameError = document.getElementById("frame-error")
+    frameError.innerHTML = ""
+    if (frames.length == 0) {
+        frameError.innerText = "You don't have any frames in this board"
+        return
+    }
+
+    const selectEl = document.getElementById("frame")
+    selectEl.innerHTML = ""
+    let selectedFrame = null
+    for (const frame of frames) {
+        if (selectedFrame === null)
+            selectedFrame = frame.id
+        const option = document.createElement("option")
+        option.value = frame.id
+        option.innerText = frame.name
+        selectEl.appendChild(option)
+    }
+    document.getElementById("select-frame").style.display = "block"
+
+    fetch("/get/board/" + boardId + "/frame/" + selectedFrame + "/objects").then(response => response.json()).then(nodes => populateNodes(boardId, selectedFrame, nodes))
+    selectEl.onchange = () => {
+        fetch("/get/board/" + boardId + "/frame/" + selectEl.value + "/objects").then(response => response.json()).then(nodes => populateNodes(boardId, selectEl.value, nodes))
+    }
+}
+
+function populateBoards(boards) {
+    const boardError = document.getElementById("board-error")
+    boardError.innerHTML = ""
+    if (boards.length == 0) {
+        boardError.innerText = "You don't have any boards"
+        return
+    }
+
+    const selectEl = document.getElementById("board")
+    selectEl.innerHTML = ""
+    let selectedBoard = null
+    for (const board of boards) {
+        if (selectedBoard === null)
+            selectedBoard = board.id
+        const option = document.createElement("option")
+        option.value = board.id
+        option.innerText = board.name
+        selectEl.appendChild(option)
+    }
+    document.getElementById("select-board").style.display = "block"
+
+    fetch("/get/board/" + selectedBoard + "/frames").then(response => response.json()).then(frames => populateFrames(selectedBoard, frames))
+    selectEl.onchange = () => {
+        fetch("/get/board/" + selectEl.value + "/frames").then(response => response.json()).then(frames => populateFrames(selectEl.value, frames))
+    }
+}
+
+fetch("/get/boards").then(response => response.json()).then(populateBoards)
